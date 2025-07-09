9b18a0e1cf755bc900e6799930b559dffbf35fd1
Alex <alexageew0506147048@gmail.com>
2025-07-09 18:13:07 +0200

    fix3

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import contactsRouter from './routers/contacts.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
-import dotenv from 'dotenv';
 
-dotenv.config();
+export const setupServer = () => {
+  const app = express();
 
-const app = express();
+  app.use(express.json());
+  app.use('/contacts', contactsRouter);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-app.use(express.json());
-
-app.use('/contacts', contactsRouter);
-
-app.use(notFoundHandler);
-app.use(errorHandler);
-
-mongoose.connect(process.env.DB_URL)
-  .then(() => {
-    app.listen(3000, () => console.log('Server running on port 3000'));
-  })
-  .catch(err => console.log(err));
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+};
