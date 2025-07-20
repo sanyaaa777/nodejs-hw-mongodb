b15f5ac4562ac807a0c5d8ee0c28bde204cb0b07
Alex <alexageew0506147048@gmail.com>
2025-07-20 17:02:04 +0200

    fix: render entry and start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import 'dotenv/config';
-import { setupServer } from './server.js';
+import './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
-  setupServer();
 };
 
 bootstrap();
