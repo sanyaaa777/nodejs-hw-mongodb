910e6670ee90e3e8024cb186006cf47dd4eb29ee
Alex <alexageew0506147048@gmail.com>
2025-08-11 19:45:51 +0200

    fix password

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -12,14 +11,32 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export function setupServer() {
   const app = express();
+
   app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
   app.use(logger);
   app.use(cors());
   app.use(cookieParser());
 
+  app.use((req, _res, next) => {
+    console.log('> ', req.method, req.originalUrl);
+    next();
+  });
+
+
   app.use('/auth', authRouter);
+
   app.get('/reset-password', (req, res) => {
-    res.status(405).json({
+    const { token = '' } = req.query;
+
+    if (process.env.NODE_ENV !== 'production') {
+      return res.json({
+        status: 200,
+        token,
+      });
+    }
+
+    return res.status(405).json({
       status: 405,
       message: 'Method Not Allowed',
       data: {
@@ -27,14 +44,15 @@ export function setupServer() {
       },
     });
   });
+
+
   app.use('/contacts', contactsRouter);
 
-  app.use(notFoundHandler);
 
+  app.use(notFoundHandler);
   app.use(errorHandler);
 
   const PORT = Number(env('PORT', 3000));
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
